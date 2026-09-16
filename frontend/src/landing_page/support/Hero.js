46021d18684b72dbf71ac03816b6b5c5407cf06d
ayeshaikh17 <ayeshashkh17june@gmail.com>
2026-09-16 14:36:03 +0530

    fixing issues

--- frontend/src/landing_page/support/Hero.js
+++ frontend/src/landing_page/support/Hero.js
@@ -3,30 +3,48 @@ import React from "react";
 function Hero() {
   return (
     <section className="container-fluid" id="supportHero">
-      <div className="p-5 " id="supportWrapper">
+      <div className="p-5" id="supportWrapper">
         <h4>Support Portal</h4>
-        <a href="">Track Tickets</a>
+        <button type="button" className="btn btn-link p-0">
+          Track Tickets
+        </button>
       </div>
+
       <div className="row p-5 m-3">
         <div className="col-6 p-3">
           <h1 className="fs-3">
             Search for an answer or browse help topics to create a ticket
           </h1>
+
           <input placeholder="Eg. how do I activate F&O" />
           <br />
-          <a href="">Track account opening</a>
-          <a href="">Track segment activation</a>
-          <a href="">Intraday margins</a>
-          <a href="">Kite user manual</a>
+
+          <button type="button" className="btn btn-link p-0 me-2">
+            Track account opening
+          </button>
+          <button type="button" className="btn btn-link p-0 me-2">
+            Track segment activation
+          </button>
+          <button type="button" className="btn btn-link p-0 me-2">
+            Intraday margins
+          </button>
+          <button type="button" className="btn btn-link p-0">
+            Kite user manual
+          </button>
         </div>
+
         <div className="col-6 p-3">
           <h1 className="fs-3">Featured</h1>
           <ol>
             <li>
-              <a href="">Current Takeovers and Delisting - January 2024</a>
+              <button type="button" className="btn btn-link p-0">
+                Current Takeovers and Delisting - January 2024
+              </button>
             </li>
             <li>
-              <a href="">Latest Intraday leverages - MIS & CO</a>
+              <button type="button" className="btn btn-link p-0">
+                Latest Intraday leverages - MIS &amp; CO
+              </button>
             </li>
           </ol>
         </div>
